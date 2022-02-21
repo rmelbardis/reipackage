c803381f7f0021d92e122a2ceb676da952cebc57
Reinis Melbardis <rmelbardis@gmail.com>
2022-02-21 12:28:25 +0000

    Initial Push

---
+++ reipackage/lib.py
@@ -0,0 +1,6 @@
+def try_me():
+    answer = input('What is the answer to life, the universe, and everything?\n')
+    if float(answer) == 42:
+        print('Congratulations, you are in posession of great wisdom!')
+    else:
+        print('Sorry, better luck next time. Good luck with your life!')
